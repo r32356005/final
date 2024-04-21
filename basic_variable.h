ad50adcf54263656acde786dd1db6eb599fb05a0
林紹恩 <154586940+shaomao94@users.noreply.github.com>
2024-04-21 22:45:46 +0800

    Create basic_variable.h

---
+++ basic_variable.h
@@ -0,0 +1,17 @@
+enum character_class{warrior,witch}; //some character classes
+
+
+//basic information of the player
+typedef struct player{              
+    char name[21];                   //player's name
+    int hp;                          //health of the player
+    int def;                         //defence of the player
+    enum character_class cc;       
+}player;
+
+
+//basic information of the monsters
+typedef struct monster{
+    char name[21];
+    int hp;
+}
