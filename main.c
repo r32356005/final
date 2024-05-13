43b6f95845deeda3328baf25da4bcfac30cf2a49
林紹恩 <154586940+shaomao94@users.noreply.github.com>
2024-05-13 22:57:05 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include "raylib.h"
+int main() {
+
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    
+    InitWindow(screenWidth, screenHeight, "Raylib basic window");
+
+    //full screen
+    ToggleFullscreen();
+    //FPS
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) {
+      BeginDrawing();
+      ClearBackground(WHITE);                      //draw the background   
+      DrawText("It works!", 50, 50, 20, BLACK);    //DrawText(TEXT,position_x,position_y,size,color)
+      EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
