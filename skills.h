d91ec8655d3b2b868ae4223b5b66346280a93dbb
林紹恩 <154586940+shaomao94@users.noreply.github.com>
2024-05-13 22:58:52 +0800

    Update skills.h

--- skills.h
+++ skills.h
@@ -1,5 +1,7 @@
 #ifndef basic_variable.h
-#define "basic_variable.h"
-#endif
 
-void warrior_skills(...);
+#define "basic_variable.h"
+void warrior_skills(...);  //戰士技能
+void witch_skills(...);    //法師技能
+
+#endif
