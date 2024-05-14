1fd6144958e18eb4b612c14410f2730865af8abe
林紹恩 <154586940+shaomao94@users.noreply.github.com>
2024-05-14 17:01:51 +0800

    Update Basic.h

--- Basic.h
+++ Basic.h
@@ -17,8 +17,9 @@ typedef struct monster{
     int def;
 }Monster;
 
-int Calculate_damage(Player *player,Monster *monster,...);            //計算傷害
-void damage(Player *player,Monster *monster,int damage);               //玩家或怪物受到傷害
+int Calculate_damage(Player *player,Monster *monster,...);             //計算傷害
+void player_damage(Player *player,int damage);        //玩家受到傷害
+void monster_damage(Monster *monster,int damage);       //怪物受到傷害
 void IsPlayerDead(Player *player);                                     //判定玩家死亡
 void IsMonsterDead(Monster *monster);                                  //判定怪物死亡
 
