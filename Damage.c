a856f48f979cceb087b02ac3acb1516cdc828620
林紹恩 <154586940+shaomao94@users.noreply.github.com>
2024-05-14 17:26:57 +0800

    Create Damage.c

---
+++ Damage.c
@@ -0,0 +1,12 @@
+int Calculate_damage(Player *player,Monster *monster,...){            //計算傷害
+
+  
+};            
+void player_damage(Player *player,int damage){                        //玩家受到傷害
+  player->hp-=damage;
+  
+};                         
+void monster_damage(Monster *monster,int damage){                     //怪物受到傷害
+  monser->hp-=damage;
+  
+};
