8a8a0c9467a8ac07b5c8f5e1a628820399261a77
yaojunchang <163412433+yaojunchang@users.noreply.github.com>
2024-05-14 17:12:08 +0800

    Update CardTypes.c

--- CardTypes.c
+++ CardTypes.c
@@ -1,1 +1,16 @@
 
+void Attack(Player *player,Monster *monster){
+
+};
+void Defense(Player *player);
+void ExtraAP(Player *player);
+
+//Warrior's skill
+void Bang(Player *player,Monster *monster);
+void Recovery(Player *player);
+void Berserker(Player *player);
+
+//Wizard's skill
+void ArmorPenetration(Monster *monster);
+void Weakness(Monster *monster);
+void StoppingTime(Monster *monster);
