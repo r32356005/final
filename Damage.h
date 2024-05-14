e4f806bbece18748a2dac55e4c5fe3f5e7e839a3
林紹恩 <154586940+shaomao94@users.noreply.github.com>
2024-05-14 17:27:48 +0800

    Update Damage.h

--- Damage.h
+++ Damage.h
@@ -1,3 +1,6 @@
+#ifndef Damage_h
+#define Damage_h
 int Calculate_damage(Player *player,Monster *monster,...);             //計算傷害
 void player_damage(Player *player,int damage);                         //玩家受到傷害
 void monster_damage(Monster *monster,int damage);                      //怪物受到傷害
+#endif
