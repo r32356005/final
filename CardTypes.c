8fea15cc0e38eac255f280d4bfbc80fa5e5233fa
yaojunchang <163412433+yaojunchang@users.noreply.github.com>
2024-05-16 17:25:08 +0800

    Update CardTypes.c

--- CardTypes.c
+++ CardTypes.c
@@ -2,19 +2,30 @@
 void Attack(Player *player,Monster *monster){
   monster.hp-=player.atk;
 };
-void Defense(Player *player);{
+void Defense(Player *player){
   player.def+=??;
 };
-void ExtraAP(Player *player);{
+void ExtraAP(Player *player){
   player.ap+=1;
 };
 //Warrior's skill
-void Bang(Player *player,Monster *monster);{
-  monster.hp-=(player.atk*2)
-void Recovery(Player *player);
-void Berserker(Player *player);
-
+void Bang(Player *player,Monster *monster){
+  monster.hp-=(player.atk*2);
+};
+void Recovery(Player *player){
+  player.hp+=??;
+};
+void Berserker(Player *player){
+  player.atk+=??;
+  player.def+=??;
+};
 //Wizard's skill
-void ArmorPenetration(Monster *monster);
-void Weakness(Monster *monster);
-void StoppingTime(Monster *monster);
+void ArmorPenetration(Monster *monster){
+  monster.def-=??;
+};
+void Weakness(Monster *monster){
+  monster.atk-=??;
+};
+void StoppingTime(Monster *monster){
+  monster.ap+=1;
+};
